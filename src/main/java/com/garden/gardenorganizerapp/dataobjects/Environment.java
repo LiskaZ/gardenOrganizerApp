4e571ef590b0775b1f15dbba69dec565463bf11c
Johannes <johannes.christmann@gmx.net>
2022-12-15 19:47:28 +0100

    add annotations to environment

--- src/main/java/com/garden/gardenorganizerapp/dataobjects/Environment.java
+++ src/main/java/com/garden/gardenorganizerapp/dataobjects/Environment.java
@@ -1,14 +1,16 @@
 package com.garden.gardenorganizerapp.dataobjects;
 
-import com.garden.gardenorganizerapp.db.DBConnection;
+import com.garden.gardenorganizerapp.dataobjects.annotations.DBEntity;
+import com.garden.gardenorganizerapp.dataobjects.annotations.DBField;
 import javafx.scene.paint.Color;
 
+@DBEntity(tableName = "Environment")
 public class Environment extends DBObject{
 
+    @DBField(name = "Name")
     private String name;
+    @DBField(name = "Defaultcolor")
     private Color color;
-    private int environmentId = DBConnection.INVALID_ID;
-
 
     public Environment() {
     }
@@ -17,7 +19,6 @@ public class Environment extends DBObject{
         this.name = name;
         this.color = color;
     }
-
 
     public void setName(String name) {
         this.name = name;
@@ -31,11 +32,4 @@ public class Environment extends DBObject{
         return color;
     }
 
-    public void setEnvironmentId(int environmentId) {
-        this.environmentId = environmentId;
-    }
-
-    public int getEnvironmentId() {
-        return environmentId;
-    }
 }
